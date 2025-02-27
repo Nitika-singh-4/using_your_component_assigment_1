42dcd6fae3758279a59c227239a87ed5074e64ef
Nitika-singh-4 <Nitikasingh262@gmail.com>
2025-02-27 10:24:21 +0530

    assignment completed

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,14 @@
 // write the book component code here
+import React from "react";
+ 
+const BookCard = ({book}) =>{
+    return(
+        <div className="book-card">
+            <h2>{book.title}</h2>
+            <p><strong>Author:</strong>{book.author}</p>
+            <p><strong>Year:</strong>{book.year}</p>
+            <p><strong>Genre:</strong>{book.genre}</p>
+        </div>
+    )
+}
+export default BookCard;
